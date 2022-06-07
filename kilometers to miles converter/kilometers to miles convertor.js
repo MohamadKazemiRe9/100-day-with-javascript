da1e8fec80dba1f48ac4b202a43fd163b10f8954
Mohammad Kazemi Renany <mohammadkazemire9@gmail.com>
2022-06-07 20:24:06 +0430

    1

---
+++ kilometers to miles converter/kilometers to miles convertor.js
@@ -0,0 +1,10 @@
+// kilometers miles converter
+// 1 kilometer equals 0.621371 mile.
+
+const kilometers = prompt("Plese enter value in kilometer");
+
+const factor = 0.621371;
+
+const miles = kilometers * factor;
+
+console.log(`${kilometers} Kilometers is equal by ${miles} miles`);
